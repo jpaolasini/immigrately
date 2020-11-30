5120b26050b52caddc6c7dfcb01c8b2c584f7ad1
Jason Paolasini <Jason.Paolasini@mcmaster.ca>
2020-11-29 21:22:43 -0500

    Genesis

---
+++ src/main/java/com/immigrately/uluru/UluruApplication.java
@@ -0,0 +1,13 @@
+package com.immigrately.uluru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UluruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UluruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immigrately/uluru/UluruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immigrately.uluru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UluruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
